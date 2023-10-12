1e7b3bf2d63d01ec8fcc71eb3fc270bfaed00a18
xuwb <xuwb@superb-pay.com>
2023-10-12 09:34:32 +0800

    修复问题 统一阿萨姆的有问题暂时用不了

--- mncjhy.py
+++ mncjhy.py
@@ -24,7 +24,7 @@ class mncjhy():
             'Host': 'h5.youzan.com',
             'Connection': 'keep-alive',
             'xweb_xhr': '1',
-            'Extra-Data': f'{"is_weapp":1,"sid":"YZ1161692133138784256YZQVT38EbS","version":"2.145.3.101024","client":"weapp","bizEnv":"wsc","uuid":"056Z2dlcvP6tQzK1697010559505","ftime":{mytool.getMSecTimestamp()}}',
+            'Extra-Data': f'{{"is_weapp":1,"sid":"YZ1161692133138784256YZQVT38EbS","version":"2.145.3.101024","client":"weapp","bizEnv":"wsc","uuid":"056Z2dlcvP6tQzK1697010559505","ftime":{mytool.getMSecTimestamp()}}}',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF XWEB/8447',
             'Content-Type': 'application/json',
             'Accept': '*/*',
@@ -65,4 +65,4 @@ if __name__ == '__main__':
         exit(0)
     else:
         for i in mytool.getlistCk(f'{tokenName}'):
-            yljf(i).login()
+            mncjhy(i).login()
