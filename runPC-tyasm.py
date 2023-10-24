a239f52bd1e2ffc240583209977c0ef67b96e330
xuwb <xuwb@superb-pay.com>
2023-10-24 13:55:54 +0800

    点击一次搜索输入框

--- runPC-tyasm.py
+++ runPC-tyasm.py
@@ -11,9 +11,11 @@ def 打开微信():
     if 寻找是否存在("pc-asm", "2.png") is False:
         raise Exception("未找到搜一搜图片")
     else:
+        点击图片中心("pc-asm", "search.png")
         输入中文('统一快乐星球')
         点击图片中心("pc-asm", "3.png")
         点击图片中心("pc-asm", "4.png", 5)
+
     点击图片中心("pc-asm", "skip.png", 5)
     点击图片中心("pc-asm", "close2.png", 5)
     if 寻找是否存在("pc-asm", "5.png", 10) is False:
@@ -24,12 +26,12 @@ def 打开微信():
 
     time.sleep(1)
     print("开始执行签到任务")
-    for i in list(pyautogui.locateAllOnScreen("pc-asm\\sign.png")):
+    for i in list(pyautogui.locateAllOnScreen(os.path.dirname(os.path.abspath(__file__)) + "\\pc-asm\\sign.png")):
         print(i)
         pyautogui.click(pyautogui.center(i))
         time.sleep(1)
     print("开始执行其他任务")
-    for i in list(pyautogui.locateAllOnScreen("pc-asm\\finish.png")):
+    for i in list(pyautogui.locateAllOnScreen(os.path.dirname(os.path.abspath(__file__)) + "\\pc-asm\\finish.png")):
         print(i)
         pyautogui.click(pyautogui.center(i))
         点击图片中心("pc-asm", "close.png")
