6cc2e54277161e1dbacad5f5e29ae3c55db5ee01
xuwb <xuwb@superb-pay.com>
2023-11-02 17:09:26 +0800

    1

--- lx.py
+++ lx.py
@@ -19,17 +19,22 @@ tokenName = 'lx_data'
 class lx():
     def __init__(self, data):
         self.sec = requests.session()
+        self.lenovoId = data[3]
         self.sec.headers = {
             'Host': 'mmembership.lenovo.com.cn',
             'Connection': 'keep-alive',
+            # 'Content-Length': '2',
             'accessToken': data[0],
             'serviceToken': f'Bearer {data[1]}',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x6309071d)XWEB/8461',
             'tenantId': '25',
             # Already added when you pass json=
             # 'Content-Type': 'application/json',
-            'SERVICE-AUTHENTICATION': 'eyJhbGciOiJIUzI1NiJ9.eyJzZXJ2aWNlTmFtZSI6IjM5MiIsInNlcnZpY2VLZXkiOiIxMzQ1MTEyZGI4YmE0MjBhYjI1MzNjOTc1NjgzNjBjNCIsInNlcnZpY2VUeXBlIjoiMSIsInNlcnZpY2VBcHAiOiIxODMiLCJzZXJ2aWNlQ2x1c3RlciI6IjEwIiwianRpIjoiOWU2ODQ0M2E2ZDFiNDllNzk1YTNlZGFmODJhZmQxNjEiLCJpYXQiOjE1NTU5MDU5NDl9.rYH1XF9xjUgW9w-4XD6OxVzal_iK3qLvPxzkPBfo0fI',
-            'lenovoId': '10270134324',
+            'SERVICE-AUTHENTICATION': data[2],
+            'clientId': '4',
+            'xweb_xhr': '1',
+            'lenovoId': self.lenovoId,
+            'Accept': '*/*',
             'Referer': 'https://servicewechat.com/wx87d0173173a58332/162/page-frame.html',
             # 'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -37,7 +42,7 @@ class lx():
 
     def login(self):
         params = {
-            'lenovoId': '10270134324',
+            'lenovoId': self.lenovoId
         }
 
         json_data = {}
@@ -61,4 +66,6 @@ if __name__ == '__main__':
         exit(0)
     else:
         for i in mytool.getlistCk(f'{tokenName}'):
+            for y in i.split('#'):
+                print(y)
             lx(i.split('#')).login()
