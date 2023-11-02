395e7f096f63544880451f5201aa86981a7495f6
xuwb <xuwb@superb-pay.com>
2023-11-02 15:02:52 +0800

    1

--- updateCookie/server.py
+++ updateCookie/server.py
@@ -33,7 +33,7 @@ def get_list_item_by_index(data_list):
     return (data_list[index])
 
 
-def updateFile(file_path, name, value):
+def addEnv(file_path, name, value):
     global 目前电话
     文件路径 = os.path.dirname(os.path.abspath(__file__)) + "\\" + 目前电话 + '-' + file_path
     if not os.path.exists(文件路径):
@@ -71,83 +71,83 @@ def logData(data: Buffer):
 @app.post("/xapi.weimob.com")
 def 统一快乐星球(data: Buffer):
     name = 'X-WX-Token'
-    updateFile(f"{data.headers['Host']}.txt", 'tyklxq_cookies', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'tyklxq_cookies', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/ucode-openapi.aax6.cn")
 def 甄爱粉俱乐部(data: Buffer):
     name = 'Authorization'
-    updateFile(f"{data.headers['Host']}.txt", 'zaf_auth', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'zaf_auth', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/m.jissbon.com")
 def 杰士邦安心福利社(data: Buffer):
     name = 'Access-Token'
-    updateFile(f"{data.headers['Host']}.txt", 'jsbaxfls', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'jsbaxfls', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/www.kozbs.com")
 def 植白说(data: Buffer):
     name = 'X-Dts-Token'
-    updateFile(f"{data.headers['Host']}.txt", 'zbsxcx', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'zbsxcx', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/kraftheinzcrm.kraftheinz.net.cn")
-def 卡夫亨(data: Buffer):
+def 卡夫味(data: Buffer):
     name = 'token'
-    updateFile(f"{data.headers['Host']}.txt", 'kfw_data', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'kfw_data', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/api.wincheers.net")
 def 罗技粉丝俱乐部(data: Buffer):
     name = 'Authorization'
-    updateFile(f"{data.headers['Host']}.txt", 'ljfsjlbCookie', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'ljfsjlbCookie', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/web.meituan.com")
 def 美团(data: Buffer):
     name = 'token'
-    updateFile(f"{data.headers['Host']}.txt", 'bd_mttoken', data.headers[name])
+    addEnv(f"{data.headers['Host']}.txt", 'bd_mttoken', data.headers[name])
     return data.headers[name]
 
 
 @app.post("/api.yqslmall.com")
 def 元气森林(data: Buffer):
     name = 'Authorization'
-    updateFile(f"{data.headers['Host']}.txt", 'yqsl', data.headers[name].replace('Bearer ', ''))
+    addEnv(f"{data.headers['Host']}.txt", 'yqsl', data.headers[name].replace('Bearer ', ''))
     return data.headers[name]
 
 
 @app.post("/apichuanti.scleader.cn")
 def 引体向上(data: Buffer):
     name = 'Authorization'
-    updateFile(f"{data.headers['Host']}.txt", 'ytxs', data.headers[name].replace('Bearer ', ''))
+    addEnv(f"{data.headers['Host']}.txt", 'ytxs', data.headers[name].replace('Bearer ', ''))
     return data.headers[name]
 
 
 @app.post("/consumer-api.quncrm.com")
 def 雀巢专业餐饮大厨精英荟(data: Buffer):
-    updateFile(f"{data.headers['Host']}.txt", 'qczy_token',
+    addEnv(f"{data.headers['Host']}.txt", 'qczy_token',
                f"{data.headers['X-Access-Token']}#{data.headers['X-Account-Id']}")
     return ""
 
 
 @app.post("/smp-api.iyouke.com")
 def meiyang会员积分(data: Buffer):
-    updateFile(f"{data.headers['Host']}.txt", 'my_auth',
+    addEnv(f"{data.headers['Host']}.txt", 'my_auth',
                data.headers['Authorization'])
     return ""
 
 
 @app.post('/app.fjxzj.com')
 def 康佰家(data: Buffer):
-    updateFile(f"{data.headers['Host']}.txt", 'kbj_token',
+    addEnv(f"{data.headers['Host']}.txt", 'kbj_token',
                data.headers['token'])
     return ""
 
@@ -156,7 +156,7 @@ def 康佰家(data: Buffer):
 def 朵茜情调生活馆(data: Buffer):
     if 'Extra-Data' in data.headers:
         extra = json.loads(data.headers['Extra-Data'])
-        updateFile(f"{data.headers['Host']}.txt", 'dqqdshgck',
+        addEnv(f"{data.headers['Host']}.txt", 'dqqdshgck',
                    f"{data.queries['access_token']}#{extra['sid']}#{extra['sid']}")
     return ""
 
@@ -164,16 +164,25 @@ def 朵茜情调生活馆(data: Buffer):
 @app.post('/api.gaojihealth.cn')
 def 高济健康(data: Buffer):
     if 'userId' in data.queries:
-        updateFile(f"{data.headers['Host']}.txt", 'wx_gjjkpro_data',
+        addEnv(f"{data.headers['Host']}.txt", 'wx_gjjkpro_data',
                    f"{data.queries['userId']}&{data.headers['Authorization']}")
     return ""
 
 
 @app.post('/channel.cheryfs.cn')
 def 好奇车生活(data: Buffer):
-    updateFile(f"{data.headers['Host']}.txt", 'hqcshck', data.headers['accountId'])
+    addEnv(f"{data.headers['Host']}.txt", 'hqcshck', data.headers['accountId'])
     return data
 
+@app.post('/yx.jsh.com')
+def 卡萨帝(data: Buffer):
+    addEnv(f"{data.headers['Host']}.txt", 'ksdck', data.headers['Authorization'])
+    return ""
+
+@app.post('/fscrm.kraftheinz.net.cn')
+def 卡夫亨(data: Buffer):
+    addEnv(f"{data.headers['Host']}.txt", 'kfh_data', data.headers['token'])
+    return ""
 
 if __name__ == '__main__':
     电话号码列表 = [
@@ -185,4 +194,4 @@ if __name__ == '__main__':
     ]
     目前电话 = get_list_item_by_index(电话号码列表)
     隐藏cmd对话框()
-    uvicorn.run(app, host="0.0.0.0", port=8989)
+    uvicorn.run("server:app", host="0.0.0.0", port=8989, reload=True)
