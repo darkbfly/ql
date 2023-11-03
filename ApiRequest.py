c9d83e8652099dbd0175bdc79a145b7e14690712
xuwb <xuwb@superb-pay.com>
2023-11-03 15:01:42 +0800

    ApiRequest

--- ApiRequest.py
+++ ApiRequest.py
@@ -1,6 +1,15 @@
 import requests
-class ApiRequest():
+
+import notify
+
+
+class ApiRequest:
     def __init__(self):
         self.sec = requests.session()
         self.sec.verify = False
         self.sec.trust_env = False
+        self.sendmsg = ''
+        self.title = ''
+
+    def send(self):
+        notify.send(self.title, self.sendmsg)
