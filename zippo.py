fa13af260cc51de8b48e68ddf794fe1cb9b36eab
xuwb <xuwb@superb-pay.com>
2024-01-08 16:49:25 +0800

    1

--- zippo.py
+++ zippo.py
@@ -2,7 +2,8 @@
 """
 cron: 30 7 * * *
 new Env("微信小程序-zippo")
-env add tpyqc_common_session_id
+env add zippo_auth
+未完成
 """
 #!/usr/bin/env python3
 # coding: utf-8
@@ -20,13 +21,14 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 title = '微信小程序-zippo'
 tokenName = 'zippo_auth'
 
+m
+
 class zippo(ApiRequest.ApiRequest):
     def __init__(self, data):
         super().__init__()
         self.sec.headers = {
             'Host': 'wx-center.zippo.com.cn',
             'Connection': 'keep-alive',
-            # 'Content-Length': '291',
             'x-app-id': 'zippo',
             'x-platform-id': 'wxaa75ffd8c2d75da7',
             'x-platform-env': 'release',
@@ -36,7 +38,6 @@ class zippo(ApiRequest.ApiRequest):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090819) XWEB/8531',
             'xweb_xhr': '1',
             'Referer': 'https://servicewechat.com/wxaa75ffd8c2d75da7/69/page-frame.html',
-            # 'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9',
         }
 
