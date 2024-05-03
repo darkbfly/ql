a8492ba178768c8c43eef9106a204d40b25a4840
darkbfly <504033589@qq.com>
2024-05-03 22:09:49 +0800

    1

--- pskj.py
+++ pskj.py
@@ -17,12 +17,10 @@ class pskj(ApiRequest.ApiRequest):
         self.sec.headers = {
             'Host': 'psjia.ipason.com',
             'Connection': 'keep-alive',
-            # 'Content-Length': '2',
             'xweb_xhr': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090a13) XWEB/9129',
             'token': data,
             'Referer': 'https://servicewechat.com/wxb0cd377dac079028/18/page-frame.html',
-            # 'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9',
         }
 
